375efe79b99f6e3141868a6e92c0dac59b473717
mariaguiar07 <m.a7faria@gmail.com>
2025-07-13 12:58:23 -0300

    JS com troca de texto ok - edição de texto errada

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
-function trocarTexto(card, novoTexto) {
-  const texto = card.querySelector('.texto-diferente');
-  const linhas = novoTexto.split('<br>');
-  texto.innerHTML = `
-    <h3>${linhas.slice(1).join('<br>')}</h3>
-  `;
+function trocarTexto(elemento, novoTexto) {
+  const textoH3 = elemento.querySelector('.texto-diferente h3');
+  textoH3.setAttribute('data-original', textoH3.innerHTML); // salva o texto original
+  textoH3.innerHTML = novoTexto;
+}
+
+function restaurarTexto(elemento) {
+  const textoH3 = elemento.querySelector('.texto-diferente h3');
+  const textoOriginal = textoH3.getAttribute('data-original');
+  if (textoOriginal) {
+    textoH3.innerHTML = textoOriginal;
+  }
 }
 
 

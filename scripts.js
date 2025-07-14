b69b75a920cd142ac120a388e6b1d7907c3a34fd
mariaguiar07 <m.a7faria@gmail.com>
2025-07-14 19:59:28 -0300

    JS funcionando, transição ok,falta alterar algumas estilizações de cards

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ function trocarTexto(elemento, novoTexto) {
   const textoH3 = elemento.querySelector('.texto-diferente h3');
   textoH3.setAttribute('data-original', textoH3.innerHTML); // salva o texto original
   textoH3.innerHTML = novoTexto;
+  textoH3.classList.add('novo-estilo'); // aplica estilo ao novo texto
 }
 
 function restaurarTexto(elemento) {
@@ -9,7 +10,9 @@ function restaurarTexto(elemento) {
   const textoOriginal = textoH3.getAttribute('data-original');
   if (textoOriginal) {
     textoH3.innerHTML = textoOriginal;
+    textoH3.classList.remove('novo-estilo'); // remove estilo para restaurar aparência
   }
 }
+
 
 

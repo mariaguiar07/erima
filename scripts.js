7c18bad9e607e6735fc807621ada8301ab14520c
mariaguiar07 <m.a7faria@gmail.com>
2025-07-15 20:44:39 -0300

    Layout finalizado - falta fazer algumas adaptações solicitadas e a responsividade

--- scripts.js
+++ scripts.js
@@ -14,5 +14,24 @@ function restaurarTexto(elemento) {
   }
 }
 
+//botão para subir a página para o topo
+function voltarAoTopo() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
 
+//para que o botão só apareça a partir da segunda seção.
+window.addEventListener('scroll', function () {
+  const segundaSecao = document.getElementById('segunda-secao');
+  const botaoTopo = document.getElementById('botao-topo');
+  const posicaoSecao = segundaSecao.getBoundingClientRect().top;
+
+  if (posicaoSecao <= 0) {
+    botaoTopo.style.display = 'block';
+  } else {
+    botaoTopo.style.display = 'none';
+  }
+});
 
